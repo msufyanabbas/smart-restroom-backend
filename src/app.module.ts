a32384dce98eef79ea6ef7befe68a86c31332e29
Muhammad Sufyan Abbas <msufyanabbas10@gmail.com>
2025-02-13 08:37:07 +0500

    added video content and CORS policy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { MqttService } from './mqtt.service';
 import { MqttController } from './mqtt.controller';
 import { WebSocketService } from './websocket.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path'; 
 
 @Module({
-  imports: [],
+  imports: [
+     ServeStaticModule.forRoot({
+      rootPath: path.join(process.cwd(), 'src', 'assets'), 
+      serveRoot: '/assets', 
+    }),
+  ],
   controllers: [AppController, MqttController],
   providers: [AppService, MqttService, WebSocketService],
 })
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,14 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // Allows all origins, change this to restrict access
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
